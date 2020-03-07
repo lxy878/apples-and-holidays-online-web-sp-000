81582299a86fddc45e088d58a022c75741f50453
Xiaoyi Liu <lxy878@gmail.com>
2020-03-07 16:22:57 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -60,7 +60,7 @@ def all_supplies_in_holidays(holiday_hash)
   # Summer:
   #   Fourth Of July: Fireworks, BBQ
   # etc.
-  
+
   holiday_hash.each do |season, holidays|
     puts "#{season}:"
     holidays.each do |holiday, supply_array|
