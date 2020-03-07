5e08a4c232ee8acde120aedd2ad7015b65e09f57
Xiaoyi Liu <lxy878@gmail.com>
2020-03-07 15:38:56 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -39,8 +39,7 @@ end
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
   # code here
   # remember to return the updated hash
-  search_hash = holiday_hash[season]
-  if !search_hash.include?(holiday_name)
+  if !holiday_hash[season].include?(holiday_name)
     holiday_hash[season][holiday_name] = []
   end
   holiday_hash[season][holiday_name] << supply_array
